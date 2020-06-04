15746a7b54453c0e4ee3c6676cc92bfbb5d5d6fa
Jan Frederick Eick <jan-frederick.eick@uni-weimar.de>
2020-06-04 16:55:14 +0200

    FIX setuptools

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open(path.join(here, 'HISTORY.rst'), encoding='utf-8') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
 requirements = [
 ]
 
@@ -25,11 +22,10 @@ test_requirements = [
 
 setup(
     name='raspyre',
-    version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     version="2.1",
     description="Raspyre Software Framework for SHM applications",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author="Jan Frederick Eick",
     author_email='jan-frederick.eick@uni-weimar.de',
     url='',
@@ -37,6 +33,7 @@ setup(
     entry_points={
         'console_scripts':[
             'raspyre-rpcserver=raspyre.rpc.server:main',
+            'raspyre-converter=raspyre.converter:main',
             ],
         },
     include_package_data=True,
